7f32c010791ba659f758cb73c928877571beed03
Alexcho0o0 <alex3452>
2017-02-06 14:23:29 +0200

    Initial commit

---
+++ ObjectsClasses/01. Day of Week/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ObjectsClasses/02. Randomize Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
