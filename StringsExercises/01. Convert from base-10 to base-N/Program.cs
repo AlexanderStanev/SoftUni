2332694bba33223ccfa1825349362fd659290d4f
Alexcho0o0 <XelaxStanev@gmailc.com>
2017-02-15 01:59:43 +0200

    Initial commit

---
+++ StringsExercises/01. Convert from base-10 to base-N/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Convert_from_base_10_to_base_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
