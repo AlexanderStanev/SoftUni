b730a187405bbd24456cb7625dc78127282f6c25
Alexcho0o0 <alex3452>
2017-02-18 19:47:35 +0200

    Initial commit

---
+++ RegexExercise/01. Extract Emails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//[ ]{1}([a-zA-Z0-9]+[.]*-*\w+@+[A-Za-z]+[-a-zA-Z]*\.{1}[a-zA-Z]+[.A-Za-z]*[-A-Za-z]*)[,.]
+
+
+            var input = Console.ReadLine();
+            var pattern = @"[ ]{1}([^ .\-_]+\.*-*\w+@+[A-Za-z]+[-a-zA-Z]*\.{1}[a-zA-Z]+[.A-Za-z]*[-A-Za-z]*)[,.]";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[1]);
+            }
+        }
+    }
+}
---
+++ RegexExercise/02. Extract sentences by keyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02.Extract_sentences_by_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentancesWordPatter = Console.ReadLine();
+            var text = Console.ReadLine();
+            var sentancesSplitPatern = @"[.!?]";
+            var wordsSplitPatter = @"\W";
+
+            Regex regex = new Regex(sentancesSplitPatern);
+
+            var sentances = Regex.Split(text, sentancesSplitPatern);
+
+            var words = new List<string>();
+            foreach (var sentance in sentances)
+            {
+                words = Regex.Split(sentance,wordsSplitPatter).ToList();
+
+                if (words.Contains(sentancesWordPatter))
+                {
+                    Console.WriteLine(sentance.Trim());
+                }
+            }
+        }
+    }
+}
