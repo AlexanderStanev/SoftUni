cf6218aee23462e195e72a06498fe76cf7e293c4
Alexcho0o0 <XelaxStanev@gmailc.com>
2017-03-02 21:32:40 +0200

    Updated project

--- ExamPreparation II/03. Nether Realms/Program.cs
+++ ExamPreparation II/03. Nether Realms/Program.cs
@@ -1,16 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _03.Nether_Realms
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var demons = Console.ReadLine().Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var demonBook = new SortedDictionary<string, string>();
 
+            foreach (var demon in demons)
+            {
+                var healthPoints = GetHealth(demon);
+                var damage = GetDamage(demon);
+
+                demonBook[demon] = $"{demon} - {healthPoints} health, {damage:F2} damage";
+            }
+
+            foreach (var demon in demonBook)
+            {
+                Console.WriteLine(demon.Value);
+            }
+        }
+
+        private static object GetDamage(string demon)
+        {
+            Regex pattern = new Regex(@"(\-|\+|)[0-9]+(\.|[0-9]|)([0-9]+|)");
+
+            var damageItems = pattern.Matches(demon);
+            double damage = 0;
+
+            foreach (var item in damageItems)
+            {
+                damage += double.Parse(item.ToString());
+            }
+
+            Regex regex = new Regex(@"[\*\/]");
+
+            var mathes = regex.Matches(demon);
+
+            foreach (var match in mathes)
+            {
+                foreach (var item in match.ToString())
+                {
+                    if (item.Equals('/'))
+                    {
+                        damage /= 2;
+                    }
+                    else
+                    {
+                        damage *= 2;
+                    }
+                }
+            }
+
+            return damage;
+        }
+
+        private static object GetHealth(string demon)
+        {
+            Regex pattern = new Regex(@"[^0-9\+\-\*\/\.]");
+
+            var healthItems = pattern.Matches(demon);
+            var healthPoints = 0;
+
+            foreach (var item in healthItems)
+            {
+                foreach (var health in item.ToString())
+                {
+                    healthPoints += health;
+                }
+            }
+
+            return healthPoints;
         }
     }
 }
---
+++ ExamPreparation II/04. Roli The Coder/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Roli_The_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var events = new Dictionary<int, Dictionary<string, List<string>>>();
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Time for Code")
+            {
+                var info = input.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+                var hashTag = info[1];
+
+                if (hashTag[0] == '#')
+                {
+                    var id = int.Parse(info[0]);
+                    var eventName = info[1].Substring(1);
+                    var participants = info.Skip(2).ToList();
+                    var participantsList = new List<string>();
+
+                    if (!events.ContainsKey(id))
+                    {
+                        for (int i = 0; i < participants.Count; i++)
+                        {
+                            if (!participantsList.Contains(participants[i]))
+                            {
+                                var nameParticipian = participants[i];
+                                participantsList.Add(nameParticipian);
+                            }
+                        }
+
+                        var innerDictionary = new Dictionary<string, List<string>>();
+                        innerDictionary.Add(eventName, participantsList);
+                        events.Add(id, innerDictionary);
+                    }
+                    else
+                    {
+                        if (events[id].ContainsKey(eventName))
+                        {
+                            for (int i = 0; i < participants.Count; i++)
+                            {
+                                if (!events[id][eventName].Contains(participants[i]))
+                                {
+                                    var participantName = participants[i];
+                                    events[id][eventName].Add(participantName);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            var result = new Dictionary<string, List<string>>();
+
+            foreach (var item in events)
+            {
+                foreach (var dict in item.Value)
+                {
+                    result.Add(dict.Key, dict.Value);
+                }
+            }
+
+            foreach (var id in result.OrderByDescending(x => x.Value.Count()).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{id.Key} - {id.Value.Count()}");
+
+                foreach (var name in id.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
